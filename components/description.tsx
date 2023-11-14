6a00c7ef5aaac7b0829da0196928d48996778025
Joshua Ochalek <pjochillin@gmail.com>
2023-11-14 00:51:04 -0500

    Add responsive design
    Update resume, add mobile design

--- components/description.tsx
+++ components/description.tsx
@@ -1,7 +1,7 @@
 export default function description() {
     return (
         <div className="z-10 mt-40 bg-dark_bg min-w-screen min-h-screen">
-            <div className="fixed mt-40 ml-[8%] mr-[8%]">
+            <div className="pt-40 ml-[8%] mr-[8%]">
                 <h1 className="text-4xl text-off_white">About</h1>
             </div>
         </div>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,13 +1,39 @@
+"use client"
+
 import Link from "next/link"
+import { useState } from "react"
 
 export default function navbar() {
+
+    const [openMobileMenu, setOpenMobileMenu] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setOpenMobileMenu(!openMobileMenu);
+    };
+
     return (
-        <div className="absolute top-0 w-full flex flex-row-reverse justfy-around">
-            <Link className = "" href="Resume.pdf">
-                <h2 className="text-4xl text-off_white inline-block transition duration-300 ease-in-out hover:text-theme_green">Resume</h2>
-            </Link>
-            <h2 className="text-4xl text-off_white inline-block mr-16 transition duration-300 ease-in-out hover:text-theme_green">Skills</h2>
-            <h2 className="text-4xl text-off_white inline-block mr-16 transition duration-300 ease-in-out hover:text-theme_green">About</h2>
-        </div>
+        <nav className="top-0 w-full">
+            <button type="button" onClick={toggleMobileMenu} className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-off_white rounded-lg md:hidden hover:bg-theme_green focus:outline-none focus:ring-2 focus:off_white dark:hover:bg-theme_green dark:focus:theme_green" aria-controls="navbar-default" aria-expanded="false">
+                <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
+                </svg>
+            </button>
+            
+            <div className={`w-full md:block md:w-auto items-center ${openMobileMenu ? 'block' : 'hidden'} transition-all duration-300 ease-in-out`}>
+                <ul className="flex flex-col md:flex-row md:justify-end md:space-x items-center space-y-4 md:space-y-0 md:space-x-16">
+                    <li>
+                        <h2 className="text-4xl text-off_white transition duration-300 ease-in-out hover:text-theme_green">About</h2>
+                    </li>
+                    <li>
+                        <h2 className="text-4xl text-off_white transition duration-300 ease-in-out hover:text-theme_green">Skills</h2>
+                    </li>
+                    <li>
+                        <Link href="resume.pdf">
+                            <h2 className="text-4xl text-off_white transition duration-300 ease-in-out hover:text-theme_green">Resume</h2>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     )
 }
