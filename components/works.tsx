39b534dbafe06cc363d0231cc0b8f56ab3251b7c
Joshua Ochalek <pjochillin@gmail.com>
2023-11-18 16:22:55 -0500

    Fix responsive design for works box

--- components/works.tsx
+++ components/works.tsx
@@ -12,16 +12,16 @@ export default function works() {
         setOpenCelloMenu(!openCelloMenu);
     };
     return (
-        <div id="works-div" className="mb-32 relative flex flex-col min-[750px]:flex-row justify-center space-x-16">
-            <div className="basis-5/12">
+        <div id="works-div" className="mx-[8%] min-[750px]:mx-0 mb-32 relative flex flex-col min-[750px]:flex-row justify-center min-[750px]:space-x-16">
+            <div className="min-[750px]:basis-5/12">
                 <h1 id="works" className="font-medium tracking-[0.3rem] text-theme_gray">— Works</h1>
                 <h1 className="pt-7 text-2xl min-[1000px]:text-4xl text-secondary_white">Activities</h1>
                 <p className="text-theme_gray text-md min-[1000px]:text-lg mt-10 min-[1000px]:mt-24 min-[1100px]:mt-16 min-[1300px]:mt-10 leading-loose">
                     Here are some current highlights to look at! Click on a panel to learn more.
                 </p>
             </div>
-            <div className="basis-5/12 flex flex-row justify-center">
-                <div className="basis-7/12">
+            <div className="min-[750px]:basis-5/12 flex flex-col min-[750px]:flex-row justify-center">
+                <div className="mt-16 min-[750px]:mt-0 min-[750px]:basis-7/12">
                     <div className={`${openCelloMenu ? "grayscale-0" : "grayscale"} hover:grayscale-0 transition duration-500 ease-in-out`} onClick={toggleOpenCelloMenu}>
                         <Image
                             src="/images/cello-thumbnail.png"
