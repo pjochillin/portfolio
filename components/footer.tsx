e2fd8477eea25bcd51434ffcf619c17222a26cce
Joshua Ochalek <pjochillin@gmail.com>
2024-01-22 18:01:13 -0500

    Update year

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,9 +8,9 @@ export default function footer() {
                 <div className="w-11/12 my-8 ml-1 bg-theme_gray h-0.5 rounded-full opacity-20"></div>
             </div>
             <div className="mb-8 flex justify-between">
-                <h3 className="text-theme_gray ml-[8%]">© Joshua Ochalek 2023</h3>
+                <h3 className="text-theme_gray ml-[8%]">© Joshua Ochalek 2024</h3>
                 <div className="mr-[8%]">
-                    
+
                     <Link href="https://github.com/pjochillin" className="inline-block">
                         <Image
                             src="/images/github-icon-gray.png"
